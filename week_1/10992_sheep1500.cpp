088e236b29ddaba0923d6e55391c2ca75b49d53b
1500sheep <cjswotl6274@naver.com>
2017-07-21 18:54:15 +0900

    [sheep1500] 10992 modifyed

--- week_1/10992_sheep1500.cpp
+++ week_1/10992_sheep1500.cpp
@@ -5,15 +5,16 @@ int main()
 	int n;
 	scanf("%d", &n);
 
+	while (n > 100 || n < 1)scanf("%d", &n);
+
 	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n*2-1; j++)
+		for (int j = 1; j <= n + i - 1; j++)
 		{
 			if (i != n) {
 				if (j == n - i + 1 || j == n + i - 1) {
 					printf("*");
 				}
 				else {
-
 					printf(" ");
 				}
 
