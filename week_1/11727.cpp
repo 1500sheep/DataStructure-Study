2cdfbdc1172acc47f19735aca3cc8e03d1d1120b
KMK <kl5549@naver.com>
2017-07-23 08:45:49 +0900

    [KMK] 11727 completed

---
+++ week_1/11727.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int D[1000];
+	D[0] = 1;
+	D[1] = 1;
+	int select;
+	cin >> select;
+
+	for (int i = 2; i <= select; i++)
+	{
+		D[i] = D[i - 1] + 2*(D[i - 2]);
+		D[i] %= 10007;
+	}
+
+	cout << D[select];
+	return 0;
+}
