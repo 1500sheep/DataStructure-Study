a2398ce2b6d4825be70260f7f6b576bbd58f8b21
1500sheep <cjswotl6274@naver.com>
2017-08-09 20:22:26 +0900

    [sheep1500] 이분탐색0 completed

---
+++ week_2/10972_sheep1500.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool next_permutation(vector<int> &a, int n) {
+	int i = n - 1;
+	while (i > 0 && a[i - 1] >= a[i])i -= 1;
+	if (i <= 0)return false;
+	int j = n - 1;
+	while (a[j] <= a[i - 1])j -= 1;
+	swap(a[i - 1], a[j]);
+	j = n - 1;
+	while (i < j) {
+		swap(a[i], a[j]);
+		i += 1;
+		j -= 1;
+	}
+	return true;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++)cin >> a[i];
+	
+	if (next_permutation(a, n)) {
+		for (int i = 0; i < n; i++) {
+			cout << a[i] << ' ';
+		}
+	}
+	else {
+		cout << "-1";
+	}
+	cout << '\n';
+
+	return 0;
+}
---
+++ week_2/10974_sheep1500.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++)a[i] = i + 1;
+
+	do {
+		for (int i = 0; i < n; i++) {
+			cout << a[i] << ' ';
+		}
+		cout << '\n';
+	}while (next_permutation(a.begin(),a.end()));
+	return 0;
+}
